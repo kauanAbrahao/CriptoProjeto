485976217e0df2280c3259acaba773a528ac3001
kauanabrahao <kauan.martins@fatec.sp.gov.br>
2021-11-17 21:15:41 -0300

    ajuste AbstractDAO erro query att mkt cap rank

--- src/main/java/com/example/CriptoProjeto/dao/AbstractDAO.java
+++ src/main/java/com/example/CriptoProjeto/dao/AbstractDAO.java
@@ -104,7 +104,7 @@ public abstract class AbstractDAO {
                         " and tvh.ID_Cripto = :idCripto;";
 
             case "updateCriptomoeda":
-                return "UPDATE Tab_Cripto SET MKT_Cap_Rank = :MKT_Cap_Rank WHERE ID_Cripto = :id_Cripto ";
+                return "UPDATE Tab_Cripto SET MKT_Cap_Rank = :MKT_Cap_Rank WHERE ID_Cripto = :ID_Cripto ";
 
             case "insertDadosHist":
                 return "INSERT INTO tab_valor_hist (ID_Cripto, Close_Price, Total_Volume, Close_Mkt_Cap, DataHR_Inc) VALUES (:ID_Cripto, :CRT_Price, :Total_Volume, :MKT_Cap, :DataHR_Inc )";
